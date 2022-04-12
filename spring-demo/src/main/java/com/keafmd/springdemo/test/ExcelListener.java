d9e9efdd17262f3843a49d652af17d1005cae74f
keafmd <674155030@qq.com>
2022-04-12 17:41:47 +0800

    2022年4月12日 完善解析表格

--- spring-demo/src/main/java/com/keafmd/springdemo/test/ExcelListener.java
+++ spring-demo/src/main/java/com/keafmd/springdemo/test/ExcelListener.java
@@ -13,66 +13,121 @@ import java.util.*;
 
 //创建读取excel监听器
 public class ExcelListener extends AnalysisEventListener<Api> {
- 
+
     //创建list集合封装最终的数据
     List<Api> list = new ArrayList<>();
- 
+    List<String> apiList = new ArrayList<>();
+    List<String> subOperationList = new ArrayList<>();
+    List<String> operationApiList = new ArrayList<>();
+
     //一行一行去读取excle内容
     @Override
     public void invoke(Api api, AnalysisContext analysisContext) {
 
-        String name = api.getFunctionPoints()+"-"+api.getFunctionPoints();
+        String name = api.getFunctionPoints() + "-" + api.getFunctionPoints();
         String functionName = api.getFunction();
 
         // function functionPoints
         // menu function
-        HashMap<String,Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
+
 
         String apiUrl = api.getUrl();
-        if(StrUtil.isNotBlank(apiUrl)) {
-            apiUrl = apiUrl.replaceAll("[\t\n\r]","");
+        if (StrUtil.isNotBlank(apiUrl)) {
+            apiUrl = apiUrl.replaceAll("[\t\n\r]", "&");
             String[] apiUrls = apiUrl.split("&");
 
-        for (String url : apiUrls) {
+            for (String url : apiUrls) {
+                if (StrUtil.isBlank(url)) {
+                    continue;
+                }
+                String uuid = RandomUtil.randomNumbers(12);
+                String id = "1513692" + uuid;
+                String[] apiStrs = url.split("、");
+
+
+                String requestName = apiStrs[0];
+                String requestMethod = apiStrs[1];
+                String requestPath = apiStrs[2];
+                //当前时间
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String createTime = format.format(new Date());
+
+
+                //  添加api
+                String sql = "INSERT INTO `ciic_iba_upms`.`upms_api`(" +
+                        "`id`, `code`, `name`, `pid`, `path`, `method`, `create_time`, `create_by`, `update_time`, `update_by`, `deleted`, `permission`, `system_id`) VALUES " +
+                        "(" + id + ", NULL, '" + requestName + "', (select id from (SELECT id from upms_api where system_id = '1513360236112338945' and name = '" + functionName + "') a), '" + requestPath + "', '" + requestMethod + "', '" + createTime + "', '1385053845579603969', NULL, NULL, 0, 2, 1513360236112338945);\n";
+//                System.out.println(sql);
+                apiList.add(sql);
+            }
+
+
+            //添加操作表数据 - 子操作
+            String subOperationName = api.getFunction()+"-"+api.getFunctionPoints();
+            String operationName = api.getFunction();
+
             String uuid = RandomUtil.randomNumbers(12);
-            String id  = "1513692"+uuid;
-            String[] apiStrs = url.split("、");
-
-
-            String requestName = apiStrs[0];
-            String requestMethod = apiStrs[1];
-            String requestPath = apiStrs[2];
-            //当前时间
+            String id = "1513701" + uuid;
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             String createTime = format.format(new Date());
 
+            String subOptionSql = "INSERT INTO `ciic_iba_upms`.`upms_operation`" +
+                    "(`id`, `pid`, `system_id`, `code`, `name`, `description`, `type`, `action`, `sort`, `icon`, `create_time`, `create_by`, `update_time`, `update_by`, `deleted`) VALUES " +
+                    "("+uuid+", "+"(select id from (SELECT id FROM `upms_operation` where `name` = '"+operationName+"') a)"+", 1513360236112338945, NULL, '"+subOperationName+"', NULL, NULL, NULL, NULL, NULL, '"+createTime+"', '1385053845579603969', NULL, NULL, 0);\n";
+
+            subOperationList.add(subOptionSql);
+//            System.out.println(subOptionSql);
 
 
-            //  添加api
-            String sql = "INSERT INTO `ciic_iba_upms`.`upms_api`(" +
-                    "`id`, `code`, `name`, `pid`, `path`, `method`, `create_time`, `create_by`, `update_time`, `update_by`, `deleted`, `permission`, `system_id`) VALUES " +
-                    "("+id+", NULL, '"+requestName+"', (select id from (SELECT id from upms_api where system_id = '1513360236112338945' and name = '"+functionName+"') a), '"+requestPath+"', '"+requestMethod+"', '"+createTime+"', '1385053845579603969', NULL, NULL, 0, 2, 1513360236112338945);\n";
-            System.out.println(sql);
+            //添加操作api表
+            for (String url : apiUrls) {
+                if (StrUtil.isBlank(url)) {
+                    continue;
+                }
+                String uuid1 = RandomUtil.randomNumbers(12);
+                String operationApiId = "1386594" + uuid1;
+                String[] apiStrs = url.split("、");
+
+
+                String requestName = apiStrs[0];
+                String requestMethod = apiStrs[1];
+                String requestPath = apiStrs[2];
+                //当前时间
+                SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String createTime1 = format.format(new Date());
+
+
+                //  添加api
+                String operationApiSql =  "INSERT INTO `ciic_iba_upms`.`upms_operation_api`" +
+                        "(`id`, `operation_id`, `api_id`, `create_time`) VALUES " +
+                        "("+operationApiId+", "+"(SELECT id FROM (SELECT id FROM `upms_operation` WHERE name = '"+subOperationName+"') a)"+", "+"(SELECT id FROM (SELECT id FROM `upms_api` WHERE name = '"+requestName+"') b)"+", '"+createTime1+"');\n";
+                operationApiList.add(operationApiSql);
+            }
+
+
+
+
         }
 
-        }
 
 
 
-
-
-
-       list.add(api);
+        list.add(api);
     }
- 
+
     //读取excel表头信息
     @Override
     public void invokeHeadMap(Map<Integer, String> headMap, AnalysisContext context) {
-        System.out.println("表头信息："+headMap);
+        System.out.println("表头信息：" + headMap);
     }
- 
+
     //读取完成后执行
     @Override
     public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+        //打印
+        apiList.forEach(System.out::println);
+        subOperationList.forEach(System.out::println);
+        operationApiList.forEach(System.out::println);
     }
 }
---
+++ src/leetcode/editor/cn/SearchInsertPosition.java
@@ -0,0 +1,77 @@
+/**
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。 
+//
+// 请必须使用时间复杂度为 O(log n) 的算法。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 5
+//输出: 2
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 2
+//输出: 1
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: nums = [1,3,5,6], target = 7
+//输出: 4
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= nums.length <= 104 
+// -104 <= nums[i] <= 104 
+// nums 为 无重复元素 的 升序 排列数组 
+// -104 <= target <= 104 
+// 
+// Related Topics 数组 二分查找 
+// 👍 1481 👎 0
+
+**/
+
+// 搜索插入位置
+// 做题时间：2022-04-12 16:44:32
+package leetcode.editor.cn;
+public class SearchInsertPosition{
+    public static void main(String[] args) {
+        Solution solution = new SearchInsertPosition().new Solution();
+        
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
+
+
